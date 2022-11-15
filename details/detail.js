0f3e07615981e9081fa2203331b3fdafeac59df7
Akilah Powell <atpjun@gmail.com>
2022-11-15 15:25:27 -0400

    created details folder and index html

---
+++ details/detail.js
@@ -0,0 +1,6 @@
+import { getCharacter } from '../fetch-utils.js';
+import { renderCharacterDetail } from '../render-utils.js';
+
+const characterDetailContainer = document.getElementById('character-detail-container');
+
+window.add
