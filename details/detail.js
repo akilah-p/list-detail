41dbe8985d40f722ed67c35f794e2e93b4b97a54
Akilah Powell <atpjun@gmail.com>
2022-11-15 15:31:49 -0400

    created the detail.js

--- details/detail.js
+++ details/detail.js
@@ -3,4 +3,13 @@ import { renderCharacterDetail } from '../render-utils.js';
 
 const characterDetailContainer = document.getElementById('character-detail-container');
 
-window.add
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+
+    const id = params.get('id');
+
+    const details = await getCharacter(id);
+
+    const getDetailEl = renderCharacterDetail(details);
+    characterDetailContainer.append(getDetailEl);
+});
